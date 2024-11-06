741ec07ba1e52481beb9d0a6be309320532bb699
Filip Rokita <filiprokita4@gmail.com>
2024-11-06 13:44:32 +0000

    cleared app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-app = flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chatterbox.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
