bd8b1ba657cf47f393dfd0822431b1f734a9255c
Filip Rokita <filiprokita4@gmail.com>
2024-12-03 14:29:29 +0100

    no t

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Description: This file contains the main code for the Flask app.t
+# Description: This file contains the main code for the Flask app.
 from flask import Flask, render_template
 from models import db, User, Message
 
