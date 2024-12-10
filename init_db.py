70b1ef2859f66f736c8a93a96afba296e68cc8fc
Filip Rokita <filiprokita4@gmail.com>
2024-12-10 15:22:51 +0000

    Add user confirmation before removing existing database file to prevent accidental data loss

--- init_db.py
+++ init_db.py
@@ -7,18 +7,19 @@ from app import app
 from models import db
 
 
-# ask the user for confirmation
-print("This script will remove the existing database file and create the database tables.")
-confirmation = input("Do you want to continue? (y/n): ")
-if confirmation.lower() != "y":
-    print("Operation cancelled")
-    exit()
-
 # database file path
 db_path = os.path.join("instance", "chatterbox.db")
 
 # check if the database file exists and remove it
 if os.path.exists(db_path):
+    # ask the user for confirmation
+    print("Existing database will be REMOVED.")
+    confirmation = input("Do you want to continue? (y/n): ")
+    if confirmation.lower() != "y":
+        print("Operation cancelled")
+        exit()
+
+    # remove the database file
     os.remove(db_path)
     print("Database file removed")
 
