564d98872015ce9a078d1a6ee8386017673b6d6a
Filip Rokita <filiprokita4@gmail.com>
2024-12-10 17:37:16 +0000

    Refactor redirect logic in helpers to use double quotes for consistency

--- helpers.py
+++ helpers.py
@@ -12,7 +12,7 @@ def login_required(f):
     def decorated_function(*args, **kwargs):
         if not session.get("user_id"):
             flash("You must be logged in to access this page.")
-            return redirect(url_for('login'))
+            return redirect(url_for("login"))
         return f(*args, **kwargs)
     return decorated_function
 
@@ -24,6 +24,6 @@ def logout_required(f):
     def decorated_function(*args, **kwargs):
         if session.get("user_id"):
             flash("You are already logged in.")
-            return redirect(url_for('chat'))
+            return redirect(url_for("chat"))
         return f(*args, **kwargs)
     return decorated_function
